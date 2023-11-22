b7b3b02774b51c8cf79a1021555b874877022aa4
Bozhidar Manev <bojomanev@gmail.com>
2023-11-22 16:01:16 +0100

    IoT device responds to server
    Co-Authored-By: Yashraj Mashruwala <105281067+Helix1579@users.noreply.github.com>
    Co-Authored-By: Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -3,10 +3,41 @@
 #include "package_builder.h"
 #include "pc_comm.h"
 #include "periodic_task.h"
+#include "wifi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "includes.h"
+
+void hello() { pc_comm_send_string_blocking("Hello, from server ;D\n"); }
 
 int main() {
+  pc_comm_init(9600, NULL);
+
+  _delay_ms(3000);
+  bool result = false;
+  wifi_init();
+
+  WIFI_ERROR_MESSAGE_t connect_to_AP =
+      wifi_command_join_AP("madinnit", "12345678");
+
+  char buffer[30];
+  if (connect_to_AP == WIFI_OK) {
+
+    pc_comm_send_string_blocking("Connected to AP!\n");
+    WIFI_ERROR_MESSAGE_t connect_to_server = wifi_command_create_TCP_connection(
+        "192.168.214.218", 23, hello, &buffer);
+  }
+  while (1)
+  {
+    /* code */
+  }
+  
+
+  /* code */
+  return 0;
+}
+
+/* int main() {
   pc_comm_init(9600, NULL);
   monitoring_system_controller_init();
   connection_controller_init();
@@ -16,64 +47,4 @@ int main() {
   }
 
   return 0;
-}
-
-/*
-
-int main() {
-  // display_init();
-  // int i = 0;
-  pc_comm_init(9600, NULL);
-  wifi_init();
-  wifi_command_join_AP("madinnit", "12345678");
-  // // WIFI_ERROR_MESSAGE_t message =
-  wifi_command_create_TCP_connection("192.168.90.98", 23, NULL, NULL);
-
-  // char buffer[100];
-  char str[44];
-  uint8_t humidity_integer, humidity_decimal, temperature_integer,
-      temperature_decimal;
-
-  while (1) {
-    if (dht11_get(&humidity_integer, &humidity_decimal, &temperature_integer,
-                  &temperature_decimal) == DHT11_OK) {
-      sprintf(str, "Humidity = %d.%d%% and the temperature = %d.%dC\n",
-              humidity_integer, humidity_decimal, temperature_integer,
-              temperature_decimal);
-      pc_comm_send_string_blocking(&str);
-      wifi_command_TCP_transmit(&str, 44);
-      _delay_ms(2000);
-    }
-
-    // pc_comm_send_string_blocking("Hello World!\n");
-    //  char message_str;
-    //  switch (message)
-    //  {
-    //  case WIFI_OK:
-    //      message_str = "Command successful.";
-    //      break;
-    //  case WIFI_FAIL:
-    //      message_str = "General failure or operation not successful.";
-    //      break;
-    //  case WIFI_ERROR_RECEIVED_ERROR:
-    //      message_str = "Received an error message from the module.";
-    //      break;
-    //  case WIFI_ERROR_NOT_RECEIVING:
-    //      message_str = "No data received from the module.";
-    //      break;
-    //  case WIFI_ERROR_RECEIVING_GARBAGE:
-    //      message_str = "Received unintelligible data from the module.";
-    //      break;
-    //  default:
-    //      message_str = "Unknown error.";
-    //      break;
-    //  }
-    //  sprintf(buffer, "message: %s\n", message_str);
-    //  pc_comm_send_string_blocking(buffer);
-    //  display_int(i++);
-    //  wifi_command_TCP_transmit(str, 51);
-  }
-  return 0;
-}
-
-*/
+} */
