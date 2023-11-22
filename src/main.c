a1493cff0605fbbb7c2cdd48aa5d903412d3a710
Bozhidar Manev <bojomanev@gmail.com>
2023-11-22 17:50:17 +0100

    Files location changed
    display_controller was in an older version of the Controller folder

--- src/main.c
+++ src/main.c
@@ -5,14 +5,18 @@
 #include "periodic_task.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include"display.h"
+#include"display_control.h"
 
 int main() {
   pc_comm_init(9600, NULL);
+  display_init();
   monitoring_system_controller_init();
   connection_controller_init();
 
-  timer_init_a(monitoring_system_controller_execute, 5000);
+  // timer_init_a(monitoring_system_controller_execute, 5000);
   while (1) {
+    display_control_write_word("Hello World");
   }
 
   return 0;
