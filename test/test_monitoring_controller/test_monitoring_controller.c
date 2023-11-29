111c40e5f4fb2894381b7f3b94694573ab07d03c
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-11-29 17:28:52 +0100

    New test scenario for monitoring controller

--- test/test_monitoring_controller/test_monitoring_controller.c
+++ test/test_monitoring_controller/test_monitoring_controller.c
@@ -8,9 +8,11 @@
 #define TEST_MONITORING_CONTROLLER
 DEFINE_FFF_GLOBALS;
 
+FAKE_VALUE_FUNC(bool, connection_controller_transmit, Package);
+FAKE_VALUE_FUNC(Package, package_builder_build_monitor, int8_t, uint8_t, uint8_t, uint16_t);
+
 
 void setUp(void) {
-    // Reset fake functions before each test
     FFF_RESET_HISTORY();
     RESET_FAKE(dht11_init);
     RESET_FAKE(dht11_get);
@@ -20,15 +22,32 @@ void setUp(void) {
 }
 
 void test_monitoring_system_controller_init(void) {
-    TEST_ASSERT_EQUAL(1, 1);
+    monitoring_system_controller_init();
+    
+    TEST_ASSERT_EQUAL_INT(1, dht11_init_fake.call_count);
+    TEST_ASSERT_EQUAL_INT(1, light_init_fake.call_count);
+}
+
+void test_monitoring_system_controller_execute_successful(void){
+    light_read_fake.return_val = 1000;
+
+    monitoring_system_controller_execute();
+    TEST_ASSERT_EQUAL_INT(1, dht11_get_fake.call_count);
+    TEST_ASSERT_EQUAL_INT(1, light_read_fake.call_count);
+    TEST_ASSERT_EQUAL_INT(1, package_builder_build_monitor_fake.call_count);
+
+    
+    TEST_ASSERT_EQUAL(DHT11_OK, dht11_get_fake.return_val_history[0]);
+    TEST_ASSERT_EQUAL_INT(1, connection_controller_transmit_fake.call_count);
 }
 
 void tearDown(void) {
-    // Clean up after each test if needed
+
 }
 
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_monitoring_system_controller_init);
+    RUN_TEST(test_monitoring_system_controller_execute_successful);
     return UNITY_END();
 }
